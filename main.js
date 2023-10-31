7fa5aa010e549ab83973ec87c6cbe2c94d4327a6
Tryhardgamerkid <126033209+Tryhardgamerkid@users.noreply.github.com>
2023-10-30 20:18:50 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,79 @@
 var SpeechRecognition = window.webkitSpeechRecognition;
-
+  
 var recognition = new SpeechRecognition();
 
 function start()
 {
-    document.getElementById("textbox").innerHTML = "";
+    document.getElementById("textbox").innerHTML = ""; 
     recognition.start();
-}
-
+} 
+ 
 recognition.onresult = function(event) {
 
-    console.log(event);
+ console.log(event); 
 
-    var Content = event.result[0][0].transcript;
-    console.log(Content);
+var Content = event.results[0][0].transcript;
 
     document.getElementById("textbox").innerHTML = Content;
     console.log(Content);
-    speak();
+      if(Content =="take my selfie")
+      {
+        console.log("taking selfie --- ");
+        speak();
+      }
 }
+
 
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_date = document.getElementById("textbox").value;
+    speak_data = "Taking your Selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
 
     synth.speak(utterThis);
+
     Webcam.attach(camera);
+
+    setTimeout(function()
+    { 
+        take_snapshot(); 
+        save();
+    }, 5000);
 }
 
+ 
+camera = document.getElementById("camera");
 Webcam.set({
     width:360,
     height:250,
-    image_format : 'png',
-    png_quality:90
+    image_format : 'jpeg',
+    jpeg_quality:90
 });
-camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    console.log(img_id);
+    
+    Webcam.snap(function(data_uri) {
+        if(img_id==selfie1);
+        document.getElementById("result1").innerHTML = '<img id="selfie1" src="'+data_uri+'"/>';
+    }
+
+        if(img_id==selfie2);
+        document.getElementById("result2").innerHTML = '<img id="selfie2" src="'+data_uri+'"/>';
+    }
+    if(img_id=="selfie3");
+    document.getElementById("result3").innerHTML = '<img id="selfie3" src="'+data_uri+'"/>';
+    }
+    });
+}
+
+
+function save()
+{
+  link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+  link.href = image;
+  link.click();
+}
